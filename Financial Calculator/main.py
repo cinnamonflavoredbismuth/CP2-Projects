0d5026ff859d9e7debce734de5f3e07f8507f1a1
cinamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-01-08 13:43:41 -0700

    began writing the main intro function

--- Financial Calculator/main.py
+++ Financial Calculator/main.py
@@ -1,5 +1,27 @@
 #Cecily Strong Financial Calculator
 #this is the financial calculator program
-
-def main():
+'''
+-How long it will take to save for a goal based on a weekly or monthly deposit
+-Compound Interest Calculator 
+-Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
+-Sale Price Calculator (apply discounts to prices)
+-Tip Calculator
+'''
+def goal(): #How long it will take to save for a goal based on a weekly or monthly deposit
     pass
+def interest():#Compound Interest Calculator
+    pass
+def budget():#Budget Allocator
+    pass
+def sale(): #Sale Price Calculator
+    pass
+def tip(): #Tip Calculator
+    pass
+def main():
+    exit=False
+    print('Welcome to your financial calculator')
+    int(input('''What would you like to do?
+              1. Calculate how long it will take to save based on a weekly or monthly deposit
+              2. Compound Interest Calculator '''))
+    if exit==False:
+        main()
