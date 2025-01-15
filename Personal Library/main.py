9113b1af095b0cd0088eb58bec45715b8b5b3032
cinamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-01-15 13:44:39 -0700

    began coding display code

--- Personal Library/main.py
+++ Personal Library/main.py
@@ -1,9 +1,40 @@
 #Cecily Strong Personal Library Program
+"""Stores all items in a list
+Function to add a new item
+Function to search the items
+Function to remove an item
+Function that runs the code (displays the menu options inside and calls the functions inside of a while True loop)
+Project must include
+easy to understand variable and function names
+Pseudocode comments explaining what the code is doing
+Good use of white space to keep items separate and easy to read/find
+Have at least 2 people test your code before submission!
+"""
+#display all items (name, author or both?) 1
+def display(library):
+    for x in library:
+        print(f"{x[1]} by {x[2]}")
+
+#search all items (by name or author) 2
+def search(library):
+    pass
+ 
+#add item (add name and author) 3
+def add(library):
+    name=input("What is the name of the book?")
+    author=input("What is the author?")
+    library.add((name,author))
+    print(library)
+
+#remove item 4
+def remove(library):
+    pass
 
 def main():
+    #how the library will work: [name,author] for each item in the library list
+    library={("The Boy Wonder #1","Juni Ba"),}
     run=True
     while run==True:
-        try:
             select=int(input("""What would you like to do?
                      1. Display all items
                      2. Search items
@@ -11,16 +42,17 @@ def main():
                      4. Remove an item
                      5. Exit"""))
             if select==1:
-                pass
+                display(library)
             elif select==2:
-                pass
+                search(library)
             elif select==3:
-                pass
+                add(library)
             elif select==4:
-                pass
+                remove(library)
             elif select==5:
                 print("Thank you")
                 run=False
                 return
-        except:
-            print("invalid option")
+
+
+main()
