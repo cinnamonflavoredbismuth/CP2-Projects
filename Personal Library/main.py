613567a2a2b6f7caa419806d215288596199c774
cinamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-01-16 13:44:05 -0700

    added \n

--- Personal Library/main.py
+++ Personal Library/main.py
@@ -13,18 +13,17 @@ Have at least 2 people test your code before submission!
 #display all items (name, author or both?) 1
 def display(library):
     for x in library:
-        print(f"{x[1]} by {x[2]}")
+        print(f"{x[0]} by {x[1]}")
 
 #search all items (by name or author) 2
 def search(library):
     pass
- 
+
 #add item (add name and author) 3
 def add(library):
-    name=input("What is the name of the book?")
-    author=input("What is the author?")
+    name=input("What is the name of the book?\n")
+    author=input("What is the author?\n")
     library.add((name,author))
-    print(library)
 
 #remove item 4
 def remove(library):
@@ -32,7 +31,7 @@ def remove(library):
 
 def main():
     #how the library will work: [name,author] for each item in the library list
-    library={("The Boy Wonder #1","Juni Ba"),}
+    library={("The Boy Wonder #1","Juni Ba"),("The Boy Wonder #2","Juni Ba")}
     run=True
     while run==True:
             select=int(input("""What would you like to do?
@@ -40,7 +39,7 @@ def main():
                      2. Search items
                      3. Add a new item
                      4. Remove an item
-                     5. Exit"""))
+                     5. Exit\n"""))
             if select==1:
                 display(library)
             elif select==2:
@@ -52,7 +51,7 @@ def main():
             elif select==5:
                 print("Thank you")
                 run=False
-                return
+                break
 
 
 main()
