1dde2d0ada0aba780729741aeeec0a21f91a3eca
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-02-06 13:43:23 -0700

    finished search function

--- Personal Library/main.py
+++ Personal Library/main.py
@@ -10,6 +10,7 @@ Pseudocode comments explaining what the code is doing
 Good use of white space to keep items separate and easy to read/find
 Have at least 2 people test your code before submission!
 """
+
 #display all items (name, author or both?) 1
 def display(library):
     for dicts in library:
@@ -18,10 +19,25 @@ def display(library):
             for keys in library[library.index(dicts)][names]:
                 print(f"    {keys}: {library[library.index(dicts)][names][keys]}") #information
 
+def find(identifier,library,books):
+    key=input(f"Who is the {identifier}?\n")
+    for dicts in library:
+        for names in dicts:
+                for keys in library[library.index(dicts)][names]:
+                    if keys==identifier:
+                        if library[library.index(dicts)][names][keys]==key:
+                                innit=True
+                                books.add(names)
+    if innit==True:
+                print(f"The comics that have this {identifier} are:")
+                print(*books, sep=', ')
+    else:
+        print(f"This is not an avaliable {identifier}")
 #search all items (by name or author) 2
 def search(library):
 
         innit=False
+        books=set()
         choose=int(input('''Press the number of what you want
         1. Title
         2. Author
@@ -35,11 +51,21 @@ def search(library):
                     print(names)
                     if name == names:
                         innit=True
-                    
             if innit==True:
-                print("It is in the library")
+                print(f"{name} is in the library")
             else:
-                print("nope")
+                print(f"{name} is not in the library")
+        elif choose==2:
+            find("author",library,books)
+            if innit==True:
+                print("The comics that have this author are:")
+                print(*books, sep=', ')
+            else:
+                print("This is not an avaliable author")
+        elif choose==3:
+            find("artist",library,books)
+            
+        
 
 
     
