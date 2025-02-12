d305985788e85b4a816329a75a1451f567fa9201
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-02-12 13:37:22 -0700

    finished search function

--- movie_recommender/main.py
+++ movie_recommender/main.py
@@ -1,10 +1,10 @@
 #Cecily Strong Movie Recommender
 """
 requierments:
-Uses the provided Movies list
-User is able to choose at least 2 filters for the program to search through
-User can get recommendations based on genre, directors, length and/or actors 
-User is able to print the whole list"""
+Uses the provided Movies list DONE
+User is able to choose at least 2 filters for the program to search through DONE
+User can get recommendations based on genre, directors, length and/or actors DONE
+User is able to print the whole list DONE"""
 import csv
 movie={}
 
@@ -24,43 +24,74 @@ with open("movie_recommender\Movies list.csv") as file:
                         "Rating": row[3],
                         "Length": row[4],
                         "Notable Actors":[row[5]]
-                         }
-    display(movie)
-def filters_setup():
-    title=""
-    director=""
-    identifiers=["director",'genre','rating','length','notable actor(s)']
-    vars=[title,director,genre,rating,length,notable_actors]
-    for x in identifiers:
-        choose=input(f"What is the {x}? type none to skip")
-        if choose.lower() != "none":
+                         }    
 
-def check(filter,titles,variable,text):
-        if len(variable)>0:
-            if (movie[titles][text]).lower()==variable.lower():
-                return(filter.append[True])
-            else: return(filter.append[False])
-        else:return(filter.append[True])
-
-def search(movie,title,director,genre,rating,length,notable_actors):
+def search(movie):
     filter=[]
     search=[]
+    exit=False
+    identifiers=["title","director",'genre','rating','length','notable actor(s)']
+    vars=["title",'director','genre','rating','length','notable_actors']
+    while exit==False:
+        try:
+            first_choice=int(input("""Press the number of what you want to do:
+            1. Print all the movies
+            2. use filters"""))
+            if first_choice==1:
+                #filter setup
+                for x in identifiers:
+                    choose=input(f"What is the {x}? type none to skip\n")
+                    if choose.lower() == "none":
+                        vars[identifiers.index(x)]=""
+                    else:
+                        vars[identifiers.index(x)]=choose
 
-    for titles in movie:
-        if len(title)>0:
-            if title.lower() == titles.lower():
-                filter.append[True]
-            else: filter.append[False]
-        else:filter.append[True]
-        check(filter,titles,director,"Director")
-        check(filter,titles,genre,"Genre")
-        check(filter,titles,rating,"Rating")
-        check(filter,titles,length,"Length")
-        if len(notable_actors)>0:
-            if notable_actors.lower() in (movie[titles]["Notable Actors"]).lower():
-                filter.append[True]
-            else: filter.append[False]
-        else:filter.append[True]
-        if False not in filter:
-            search.append(titles)
-    return(search)
+                #checking all the movies using the filters
+                for titles in movie:
+                    #title
+                    if len(vars[0])>0:
+                        if vars[0].lower() == titles.lower():
+                            filter.append[True]
+                        else: filter.append[False]
+                    else:filter.append[True]
+                    #director,genre,rating,length
+                    for x in vars[1:4]:
+                        if len(vars)>0:
+                            if (movie[titles][identifiers[vars.index[x]]]).lower()==vars.lower():
+                                return(filter.append[True])
+                            else: return(filter.append[False])
+                        else:return(filter.append[True])
+                    #notable actors
+                    if len(vars[5])>0:
+                        if vars[5].lower() in (movie[titles]["Notable Actors"]).lower():
+                            filter.append[True]
+                        else: filter.append[False]
+                    else:filter.append[True]
+                    #adds the movie to the list of the correct results
+                    if False not in filter:
+                        search.append(titles)
+                return(search)
+            elif first_choice==2:
+                return(movie)
+            else:
+                print("invalid choice")
+        except:
+            print("Invalid choice")
+
+def main(movie):
+    stay=True
+    while stay==True:
+        try:
+            choice=int(input("""choose the number of what to do
+            1. search movies
+            2. exit"""))
+                
+            if choice==1:
+                filtered=search(movie)
+                display(filtered)
+            elif choice==2:
+                return
+            else:
+                print("invalid choice")
+        except:
+            print("invalid choice")
