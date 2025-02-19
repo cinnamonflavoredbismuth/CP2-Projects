e0eca3c0f5d6d7f63937c12214265f3aad01687f
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-02-19 13:44:04 -0700

    ggag

--- Notes UwU/write to files notes/main.py
+++ Notes UwU/write to files notes/main.py
@@ -9,17 +9,18 @@ x = create a file
 a+ = append and read
 """
 import csv
+#import pandas
 
-data=[
+'''data=[
     {"username":'hi','color':'blue'}
     ]
 with open("Notes UwU/reading_files/user_info.csv","a",newline="") as file:
     fieldnames=['']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
-    writer.writerows(data)
+    writer.writerows(data)'''
 
 with open("Notes UwU/reading_files/user_info.csv","r") as file:
     reader = csv.reader(file)
     for row in reader:
-        print(f"{row[0]}\n{row[1]} \n ----------------------------")
+        print(f"{reader.id(row)}: {row[0]}\n{row[1]} \n ----------------------------")
--- to do list/main.py
+++ to do list/main.py
@@ -3,3 +3,33 @@
 Add items to the to do list
 Mark item as complete
 Delete item from to do list'''
+import csv
+
+import pandas as pd
+
+def display():
+    with open("to do list/list.csv","r") as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            print(f" {row[0]} {row[1]} \n ----------------------------")
+def add():
+    pass
+
+def complete():
+    pass
+
+def delete(number):
+    data = {}
+    with open("to do list/list.csv","r+") as file:
+        reader = csv.reader(file)
+        print(reader)
+        for row in reader:
+            if row != number:
+                data[]
+        print(reader)
+
+def main():
+    delete()
+
+main()
