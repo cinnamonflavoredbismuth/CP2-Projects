acabe674d89abfd5c6e8e9130156921813503d1f
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-02-20 13:44:13 -0700

    I tried

---
+++ silly/silly.py
@@ -0,0 +1,15 @@
+#gay
+row=1
+import csv
+
+[[[]]]
+with
+"''"
+()()
+print
+####
+range
+def
+row
+===
+[[[]]]
--- to do list/main.py
+++ to do list/main.py
@@ -12,24 +12,42 @@ def display():
         reader = csv.reader(file)
         next(reader)
         for row in reader:
-            print(f" {row[0]} {row[1]} \n ----------------------------")
-def add():
-    pass
+            print(f" {row[0]}: {row[1]} {row[2]} \n ----------------------------")
+def setup():
+    basics={
+        "number": [1,2],
+        "items": ["wash dishes",'mow lawn'],
+        'completion status': ['','X']
+        }
+    with open("to do list/list.csv","w+") as file:
+        
+        writer = csv.DictWriter(file, fieldnames = basics.keys())
+        writer.writeheader()
+        for i in range(len(basics)):
+            writer.writerow([basics[x][i]])
+        print(writer)
 
 def complete():
     pass
 
-def delete(number):
+def all_data(number):
     data = {}
-    with open("to do list/list.csv","r+") as file:
+    with open("to do list/list.csv","r") as file:
         reader = csv.reader(file)
-        print(reader)
+        #next(reader)
         for row in reader:
-            if row != number:
-                data[]
-        print(reader)
-
+                if row != number:
+                    data[row[0]]=[row[1],row[2]]
+        print(data)
+        return(data)
+            
+def delete(data): 
+    with open("to do list/list.csv","w+") as file:
+        writer = csv.DictWriter(file, fieldnames = data)
+        writer.writerows(data)
+        print(writer)
 def main():
-    delete()
+    setup()
+  #  delete(all_data(1))
 
 main()
